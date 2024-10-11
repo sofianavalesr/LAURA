ecdac55cb2ec0542c994643620ca18f9150969a6
Laura Laguado Echeverri <lalaguado17@gmail.com>
2024-10-10 19:24:47 -0500

    Cambios en inventario

--- inventario.cs
+++ inventario.cs
@@ -1,13 +1,46 @@
 using System.Collections.Generic;
-
+using System.IO;
 
 public class Inventario
 {
     public List<Producto> Productos { get; set; } = new List<Producto>();
 
-
     public void ImprimirEstadoInventario()
     {
         Producto.ImprimirEstadoInventario(Productos);
+    }
+
+    // Método para guardar el inventario en un archivo CSV
+    public void GuardarInventario(string rutaArchivo)
+    {
+        using (StreamWriter writer = new StreamWriter(rutaArchivo))
+        {
+            writer.WriteLine("Id,Nombre,Precio,Cantidad"); // Encabezados
+            foreach (var producto in Productos)
+            {
+                writer.WriteLine($"{producto.Id},{producto.Nombre},{producto.Precio},{producto.Cantidad}");
+            }
+        }
+    }
+
+    // Método para cargar el inventario desde un archivo CSV
+    public void CargarInventario(string rutaArchivo)
+    {
+        if (File.Exists(rutaArchivo))
+        {
+            var lineas = File.ReadAllLines(rutaArchivo);
+            for (int i = 1; i < lineas.Length; i++) // Saltar el encabezado
+            {
+                var columnas = lineas[i].Split(',');
+                var producto = new Producto
+                {
+                    Id = int.Parse(columnas[0]),
+                    Nombre = columnas[1],
+                    Precio = decimal.Parse(columnas[2]),
+                    Cantidad = int.Parse(columnas[3])
+                };
+                Productos.Add(producto);
+            }
+        }
     }
 }
